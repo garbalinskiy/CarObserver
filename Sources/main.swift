526feb30decfd098f674df43ea6c378877ce7337
Sergey Garbalinskiy <garbalinskiy@me.com>
2017-03-20 19:37:55 +0300

    Removed unnecessary semaphore

--- Sources/main.swift
+++ Sources/main.swift
@@ -1,16 +1,10 @@
 import Foundation
 
-var sema = DispatchSemaphore( value: 0 )
-
 let engine = COEngine()
 
 engine.addParser(AVBYParser())
 
-DispatchQueue(label: "checkoutQueue").async {
-    while true {
-        engine.checkout()
-        sleep(3)
-    }
+while true {
+    engine.checkout()
+    sleep(3)
 }
-
-sema.wait()
